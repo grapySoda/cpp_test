6b526a49301b2fb45913a70b76adbecc06f4eeaa
grapysoda <chihhsing_chen@innodisk.com>
2024-02-27 11:37:18 +0800

    feat: mutilsink logger

--- main.cpp
+++ main.cpp
@@ -3,15 +3,34 @@
 
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 
 int main()
 {
+    int i = 0;
     std::string str;
-    auto console_logger = spdlog::stdout_logger_mt("console");
+    // auto console_logger = spdlog::stdout_logger_mt("console");
+
+    auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+    console_sink->set_level(spdlog::level::warn);
+    console_sink->set_pattern("[multi_sink_example] [%^%l%$] %v");
+
+    auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(
+        "logs/multisink.txt", true);
+    file_sink->set_level(spdlog::level::trace);
+
+    spdlog::logger logger("multi_sink", {console_sink, file_sink});
+    logger.set_level(spdlog::level::debug);
 
     while (true) {
+        ++i;
         std::cin >> str;
-        console_logger->info(str);
+        logger.warn(str);
+        if (i > 5) {
+            logger.flush();
+            i = 0;
+        }
     }
 
     return 0;
